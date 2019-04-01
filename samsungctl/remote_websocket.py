c308017cb0e79f75f39507e3b366bdb79e2a1217
George Botros <botrosg@rose-hulman.edu>
2019-04-01 17:05:31 -0400

    updated using a newer version of the samsungctl websockets file

--- samsungctl/remote_websocket.py
+++ samsungctl/remote_websocket.py
@@ -3,29 +3,32 @@ import json
 import logging
 import socket
 import time
+import ssl
 
 from . import exceptions
 
-
-URL_FORMAT = "ws://{}:{}/api/v2/channels/samsung.remote.control?name={}"
+URL_FORMAT = "wss://{}:{}/api/v2/channels/samsung.remote.control?name={}"
 
 
 class RemoteWebsocket():
     """Object for remote control connection."""
 
     def __init__(self, config):
+        self.token = ""
         import websocket
 
         if not config["port"]:
-            config["port"] = 8001
+            config["port"] = 8002
 
         if config["timeout"] == 0:
             config["timeout"] = None
 
         url = URL_FORMAT.format(config["host"], config["port"],
                                 self._serialize_string(config["name"]))
+        if config["token"]:
+            url += "&token=" + config["token"]
 
-        self.connection = websocket.create_connection(url, config["timeout"])
+        self.connection = websocket.create_connection(url, ssl_handshake_timeout=config["timeout"], sslopt={"cert_reqs": ssl.CERT_NONE})
 
         self._read_response()
 
@@ -72,6 +75,8 @@ class RemoteWebsocket():
             raise exceptions.UnhandledResponse(response)
 
         logging.debug("Access granted.")
+        if response.get("token", False):
+            logging.info("Token: {}".format(response["token"]))
 
     @staticmethod
     def _serialize_string(string):
